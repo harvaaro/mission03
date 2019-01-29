1ed18fa372bfbaa9e9e0c4e0de425f43de5485d6
harvaaro <harvaaro@isu.edu>
2019-01-29 10:44:36 -0700

    Setup to build now, but need to copy over my previous linked stack and such into this one.

---
+++ src/main/java/edu/isu/cs/cs3308/structures/impl/LinkedQueue.java
@@ -0,0 +1,50 @@
+package edu.isu.cs.cs3308.structures.impl;
+
+import edu.isu.cs.cs3308.structures.Queue;
+
+public class LinkedQueue<E> implements Queue<E> {
+	@Override
+	public int size() {
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return false;
+	}
+
+	@Override
+	public void offer(E element) {
+
+	}
+
+	@Override
+	public E peek() {
+		return null;
+	}
+
+	@Override
+	public E poll() {
+		return null;
+	}
+
+	@Override
+	public void printQueue() {
+
+	}
+
+	@Override
+	public void transfer(Queue<E> into) {
+
+	}
+
+	@Override
+	public void reverse() {
+
+	}
+
+	@Override
+	public void merge(Queue<E> from) {
+
+	}
+}
