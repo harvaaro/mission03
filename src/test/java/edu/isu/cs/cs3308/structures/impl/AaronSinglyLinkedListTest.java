a88eda1a0826843301dd008a93ac36a47eae3633
harvaaro <harvaaro@isu.edu>
2019-02-03 12:53:02 -0700

    I think I was able to make it pretty well as a more automated approach. I think think it is wrong in places but will need to check later on.

--- src/test/java/edu/isu/cs/cs3308/structures/impl/AaronSinglyLinkedListTest.java
+++ src/test/java/edu/isu/cs/cs3308/structures/impl/AaronSinglyLinkedListTest.java
@@ -3,6 +3,9 @@ package edu.isu.cs.cs3308.structures.impl;
 import org.junit.*;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 import static org.junit.Assert.*;
 
@@ -15,6 +18,9 @@ public class AaronSinglyLinkedListTest {
 	// Initial setup
 	//	/	/	/	/
 	private SinglyLinkedList<Integer> fixture;
+//	private DoublyLinkedList<Integer> fixture;
+
+	private ArrayList<Integer> verifyList;
 
 	private String newln = System.lineSeparator();
 	private String sepln = newln + "###### ###### ######";
@@ -25,7 +31,9 @@ public class AaronSinglyLinkedListTest {
 	private int value5 = 50;
 	private int valueInsert = 12345;
 
-	private int[] addValues = {10, 20, 30, 40, 50, 60};
+	private int[] addValues = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+
+	private int rndBound = 10;
 	private String testing = newln + "Testing --\t";
 	private String shouldBe = newln + "Should be:\t";
 	private String listSize = newln + "List size:\t";
@@ -66,6 +74,9 @@ public class AaronSinglyLinkedListTest {
 	@Before
 	public void setUp() {
 		fixture = new SinglyLinkedList<>();
+//		fixture = new DoublyLinkedList<>();
+
+		verifyList = new ArrayList<>();
 	}
 
 	@After
@@ -219,12 +230,167 @@ public class AaronSinglyLinkedListTest {
 		testPrint(printList);
 	}
 
+	private void methodLoopChoice(int orderIndex, boolean doAdd) {
+		Random rnd = new Random();
+		int addValue = rnd.nextInt(10)+rndBound;
+		rndBound += 10;
+
+		if (orderIndex == 0) {
+			if (doAdd) {
+				fixture.addFirst(addValue);
+				verifyList.add(0, addValue);
+				tempListSize++;
+			}
+			else {
+				fixture.removeFirst();
+				verifyList.remove(0);
+				tempListSize--;
+			}
+		}
+		else if (orderIndex == 1) {
+			if (doAdd) {
+				fixture.addLast(addValue);
+				verifyList.add(verifyList.size(), addValue);
+				tempListSize++;
+			}
+			else {
+				fixture.removeLast();
+				verifyList.remove(verifyList.size()-1);
+				tempListSize--;
+			}
+		}
+		else if (orderIndex == 2) {
+			if (doAdd) {
+				fixture.insert(addValue, 0);
+				verifyList.add(0, addValue);
+				tempListSize++;
+			}
+			else {
+				fixture.remove(0);
+				verifyList.remove(0);
+				tempListSize--;
+			}
+		}
+		else if (orderIndex == 3) {
+			if (doAdd) {
+				fixture.insert(addValue, 1);
+				if (1 > trueListSize) {
+					verifyList.add(0, addValue);
+					tempListSize++;
+				}
+				else {
+					verifyList.add(1, addValue);
+					tempListSize++;
+				}
+			}
+			else {
+				fixture.remove(1);
+				if (1 < trueListSize) {
+					verifyList.remove(1);
+					tempListSize--;
+				}
+			}
+		}
+		else if (orderIndex == 4) {
+			if (doAdd) {
+				fixture.insert(addValue, 2);
+				if (2 > trueListSize) {
+					verifyList.add(1, addValue);
+					tempListSize++;
+				}
+				else {
+					verifyList.add(2, addValue);
+					tempListSize++;
+				}
+			}
+			else {
+				fixture.remove(2);
+				if (2 < trueListSize) {
+					verifyList.remove(2);
+					tempListSize--;
+				}
+			}
+		}
+		else if (orderIndex == 5) {
+			if (doAdd) {
+				fixture.insert(addValue, 3);
+				if (3 > trueListSize) {
+					verifyList.add(2, addValue);
+					tempListSize++;
+				}
+				else {
+					verifyList.add(3, addValue);
+					tempListSize++;
+				}
+			}
+			else {
+				fixture.remove(3);
+				if (3 < trueListSize) {
+					verifyList.remove(3);
+					tempListSize--;
+				}
+			}
+		}
+		else if (orderIndex == 6) {
+			if (doAdd) {
+				fixture.insert(addValue, 4);
+				if (4 > trueListSize) {
+					verifyList.add(3, addValue);
+					tempListSize++;
+				}
+				else {
+					verifyList.add(4, addValue);
+					tempListSize++;
+				}
+			}
+			else {
+				fixture.remove(4);
+				if (4 < trueListSize) {
+					verifyList.remove(4);
+					tempListSize--;
+				}
+			}
+		}
+		else if (orderIndex == 7)  {
+			if (doAdd) {
+				fixture.insert(addValue, 5);
+				if (5 > trueListSize) {
+					verifyList.add(4, addValue);
+					tempListSize++;
+				}
+				else {
+					verifyList.add(5, addValue);
+					tempListSize++;
+				}
+			}
+			else {
+				fixture.remove(5);
+				if (5 < trueListSize) {
+					verifyList.remove(5);
+					tempListSize--;
+				}
+			}
+		}
+		else{
+			if (doAdd) {
+				fixture.insert(addValue, -1);
+				verifyList.add(-1, addValue);
+			}
+			else {
+				fixture.remove(-1);
+				verifyList.remove(-1);
+			}
+		}
+	}
+
 	private void checkSize() {
 		trueListSize = tempListSize;
 		printResult = "";
 
-		int[] listValues = addValues;
+		int[] listValues = new int[trueListSize];
+
 		for (int i = 0; i < listValues.length; i++) {
+			listValues[i] = verifyList.get(i);
 			printResult += newln + listValues[i];
 		}
 
@@ -247,130 +413,142 @@ public class AaronSinglyLinkedListTest {
 		testNormal("isEmpty()",					fixture.isEmpty());
 		trueResult = (trueListSize > 0) ? listValues[0] : null;
 		testNormal("first()",					fixture.first());
-		testNormal("get("+testIndex0+")",		fixture.get(testIndex0)); //TODO
+		testNormal("get("+0+")",					fixture.get(0));
 		trueResult = lastTrueValue;
 		testNormal("last()",						fixture.last());
-		testNormal("get("+testIndex1+")",		fixture.get(testIndex1));
+		testNormal("get("+(trueListSize-1)+")",	fixture.get(trueListSize-1));
 		trueResult = null;
-		testNormal("get("+testIndexNeg+")",		fixture.get(testIndexNeg));
-		testNormal("get("+testIndex2+")",		fixture.get(testIndex2));
+		testNormal("get("+(-1)+")",				fixture.get(-1));
+		testNormal("get("+trueListSize+")",		fixture.get(trueListSize));
 
-		for (int i = 0; i < tempListSize; i++) {
-			trueResult = i;
-			testNormal("indexOf(" + listValues[i] + ")", fixture.indexOf(listValues[i]));
+		if (trueListSize > 2) {
+			for (int i = 1; i < trueListSize; i++) {
+				trueResult = listValues[i];
+				testNormal("get("+i+")",			fixture.get(i));
+			}
+		}
+
+		if (trueListSize == 0) {
+			trueResult = null;
+			testNormal("removeFirst()",			fixture.removeFirst());
+			testNormal("removeLast()", 			fixture.removeLast());
+			testNormal("remove(" + (-1) + ")",	fixture.remove(-1));
+			testNormal("remove(" + 0 + ")",		fixture.remove(0));
+			testNormal("remove(" + 1 + ")",		fixture.remove(1));
+			trueResult = -1;
+			testNormal("indexOf(" + addValues[0] + ")", fixture.indexOf(addValues[0]));
+		}
+		else {
+			for (int i = 0; i < tempListSize; i++) {
+				trueResult = i; //TODO check for -1 maybe
+				testNormal("indexOf(" + listValues[i] + ")", fixture.indexOf(listValues[i]));
+			}
 		}
 
 		trueResult = printResult;
 		testPrint(listValues);
 	}
 
-	private void methodLoopChoice(int orderIndex, boolean doAdd) {
-		if (orderIndex == 0) {
-			if (doAdd) {
-				fixture.addFirst(addValues[orderIndex]);
-			}
-			else {
-				fixture.removeFirst();
-			}
+	private void checkAddRemove(int checkListSize) {
+		// add appropriate amounts for checking
+		for(int i = 0; i < checkListSize-1; i++) {
+			methodLoopChoice(1, true);
 		}
-		else if (orderIndex == 1) {
-			if (doAdd) {
-				fixture.addLast(addValues[orderIndex]);
-			}
-			else {
-				fixture.removeLast();
-			}
+
+		int insertAmounts = checkListSize + 4;
+
+		String[] addOrder = {"addFirst()", "addLast", "insert(0)", "insert(1)",
+				"insert(2)", "insert(3)", "insert(4)", "insert(5)", "insert(-1)"};
+		String[] removeOrder = {"removeFirst()", "removeLast()", "remove(0)", "remove(1)",
+				"remove(2)", "remove(3)", "remove(4)", "remove(5)", "remove(-1)"};
+
+		System.out.println(sepln + newln + "List Size " + checkListSize + sepln);
+
+		if (checkListSize == 0) {
+			checkSize();
 		}
-		else if (orderIndex == 2) {
-			if (doAdd) {
-				fixture.insert(addValues[orderIndex], 0);
-			}
-			else {
-				fixture.remove(0);
-			}
-		}
-		else if (orderIndex == 3) {
-			if (doAdd) {
-				fixture.insert(addValues[orderIndex], 1);
-			}
-			else {
-				fixture.remove(1);
-			}
-		}
-		else if (orderIndex == 4) {
-			if (doAdd) {
-				fixture.insert(addValues[orderIndex], 2);
-			}
-			else {
-				fixture.remove(2);
-			}
-		}
-		else if (orderIndex == 5) {
-			if (doAdd) {
-				fixture.insert(addValues[orderIndex], 3);
-			}
-			else {
-				fixture.remove(3);
-			}
-		}
-		else if (orderIndex == 6) {
-			if (doAdd) {
-				fixture.insert(addValues[orderIndex], 4);
-			}
-			else {
-				fixture.remove(4);
-			}
-		}
-		else if (orderIndex == 7)  {
-			if (doAdd) {
-				fixture.insert(addValues[orderIndex], 5);
-			}
-			else {
-				fixture.remove(5);
-			}
-		}
-		else{
-			if (doAdd) {
-				fixture.insert(addValues[orderIndex], -1);
-			}
-			else {
-				fixture.remove(-1);
+		else {
+			for (int i = 0; i < insertAmounts; i++) {
+				whichInsert = addOrder[i];
+				whichRemove = "";
+				System.out.println(sepln + newln + whichInsert + whichRemove + sepln);
+
+				methodLoopChoice(i, true);
+//		if (i != 8) { tempListSize++; }
+				checkSize();
+
+				whichRemove = removeOrder[i];
+				System.out.println(sepln + newln + whichInsert + whichRemove + sepln);
+
+				methodLoopChoice(i, false);
+//		if (i != 8) { tempListSize++; }
+				checkSize();
 			}
 		}
 	}
 
-	private void checkAddRemove(int checkListSize) {
-		// add appropriate amounts for checking
-		for(int i = 0; i < checkListSize -1; i++) {
-			fixture.addLast(addValues[i]);
-		}
+	//	/	/	/	/
+	// Actual tests
+	//	/	/	/	/
 
-		int removeAmounts = checkListSize + 4;
-		int insertAmounts = removeAmounts;
-		int amountPerLoop = removeAmounts;
+	/**
+	 * Test of new, of class SinglyLinkedList.
+	 */
+	@Test
+	public void ListSize0() {
+		totalTally = 15;
 
-		String[] addOrder = {"addFirst()", "addLast", "insert(-1)", "insert(0)",
-				"insert(1)", "insert(2)", "insert(3)", "insert(4)", "insert(5)"};
-		String[] removeOrder = {"removeFirst()", "removeLast()", "remove(0)",
-				"remove(1)", "remove(2)", "remove(3)", "remove(4)", "remove(5)", "remove(-1)"};
+		checkAddRemove(0);
+	}
 
-		System.out.println(sepln + newln + "List Size " + checkListSize + sepln);
+	/**
+	 * Test of new, of class SinglyLinkedList.
+	 */
+	@Test
+	public void ListSize1() {
+		totalTally = 117;
 
-		for (int i = 0; i < insertAmounts; i++) {
-			whichInsert = addOrder[i];
-			whichRemove = "";
+		checkAddRemove(1);
+	}
 
-			System.out.println(sepln + newln + whichInsert + sepln);
+	/**
+	 * Test of new, of class SinglyLinkedList.
+	 */
+	@Test
+	public void ListSize2() {
+		totalTally = 14;
 
-			methodLoopChoice(i,true);
-			tempListSize++;
-			checkSize();
+		checkAddRemove(2);
+	}
 
-			whichRemove = removeOrder[i];
-			methodLoopChoice(i,false);
-			tempListSize--;
-			checkSize();
-		}
+	/**
+	 * Test of new, of class SinglyLinkedList.
+	 */
+	@Test
+	public void ListSize3() {
+		totalTally = 14;
+
+		checkAddRemove(3);
+	}
+
+	/**
+	 * Test of new, of class SinglyLinkedList.
+	 */
+	@Test
+	public void ListSize4() {
+		totalTally = 14;
+
+		checkAddRemove(4);
+	}
+
+	/**
+	 * Test of new, of class SinglyLinkedList.
+	 */
+	@Test
+	public void ListSize5() {
+		totalTally = 14;
+
+		checkAddRemove(5);
 	}
 
 	/**
